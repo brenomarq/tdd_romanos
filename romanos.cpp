f057ddf0b61f829f488e8ed2fbcdf2964a9576f6
brenomarq <brenomarques516@gmail.com>
2024-04-25 13:06:33 -0300

    Trying to implement the logic

--- romanos.cpp
+++ romanos.cpp
@@ -1,6 +1,27 @@
 #include "romanos.hpp"
-int romanos_para_decimal(char const * num_romano)
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int romanos_para_decimal(const string num_romano)
 {
-  return 0; 
+  unordered_map<char, int> algarismos = {
+    {'I', 1},
+    {'V', 5},
+    {'X', 10},
+    {'L', 50},
+    {'C', 100},
+    {'D', 500},
+    {'M', 1000},
+  };
+
+  int resultado = 0;
+  int valorPrevio = 0;
+
+  for(int i = 0; i < num_romano.size(); ++i) {
+
+  }
+
+  return 0;
 }
- 
