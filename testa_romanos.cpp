df8294d5e49cb9a10fe8b0957f2bdb67d90e498b
brenomarq <brenomarques516@gmail.com>
2024-04-25 16:12:20 -0300

    Add more test cases

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -7,18 +7,22 @@ TEST_CASE( "Numeros romanos - algarismos únicos", "[romanos]" ) {
     REQUIRE( romanos_para_decimal("I") == 1 );
 
     REQUIRE( romanos_para_decimal("V") == 5 );
-
-
 }
 
+TEST_CASE( "Numeros romanos - algarismos compostos", "[romanos]" ) {
+    REQUIRE( romanos_para_decimal("IV") == 4 );
 
+    REQUIRE( romanos_para_decimal("XL") == 40 );
+
+    REQUIRE( romanos_para_decimal("VII") == 7 );
+
+    REQUIRE( romanos_para_decimal("XC") == 90 );
+}
 
 
 TEST_CASE( "Numeros romanos - algarismos inválidos", "[romanos]" ) {
     REQUIRE( romanos_para_decimal("G") == -1 );
 
     REQUIRE( romanos_para_decimal("i") == -1 );
-
-
 }
 
